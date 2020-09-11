d3084a98225e8449e9b61378dd0936d1f2ad9380
mehdi <mehdi@172.19.170.45>
2020-09-11 17:03:55 +0200

    projet

--- cpoa_akniou/src/cpoa_akniou/tCategorie.java
+++ cpoa_akniou/src/cpoa_akniou/tCategorie.java
@@ -13,8 +13,7 @@ public class tCategorie{
 			Connection laConnexion = Connexion.creeConnexion();
 			Statement requete = laConnexion.createStatement();
 			requete.executeUpdate("INSERT INTO `akniou1u_cpoa`.`Categorie` (`id_categorie`, `titre`, `visuel`) VALUES ('"+no_cat+"', '"+titre+"', '"+visuel+"');");
-			ResultSet res = requete.executeQuery("SELECT * FROM `akniou1u_cpoa`.`Categorie`");
-			affCategorie(res);
+			affCategorie();
 			requete.close();
 			laConnexion.close();
 		} catch (SQLException sqle) {
@@ -27,8 +26,7 @@ public class tCategorie{
 			Connection laConnexion = Connexion.creeConnexion();
 			Statement requete = laConnexion.createStatement();
 			requete.executeUpdate("DELETE FROM `akniou1u_cpoa`.`Categorie` WHERE `Categorie`.`id_categorie` = "+no_cat+"");
-			ResultSet res = requete.executeQuery("SELECT * FROM `akniou1u_cpoa`.`Categorie`");
-			affCategorie(res);
+			affCategorie();
 			requete.close();
 			laConnexion.close();
 		} catch(SQLException sqle) {
@@ -41,8 +39,7 @@ public class tCategorie{
 			Connection laConnexion = Connexion.creeConnexion();
 			Statement requete = laConnexion.createStatement();
 			requete.executeUpdate("UPDATE `akniou1u_cpoa`.`Categorie` SET `titre` = '"+titre+"', `visuel` = '"+visuel+"' WHERE `Categorie`.`id_categorie` = "+no_cat+";");
-			ResultSet res = requete.executeQuery("SELECT * FROM `akniou1u_cpoa`.`Categorie`");
-			affCategorie(res);
+			affCategorie();
 			requete.close();
 			laConnexion.close();
 		} catch(SQLException sqle) {
@@ -50,15 +47,20 @@ public class tCategorie{
 		}
 	}
 	
-	private static void affCategorie(ResultSet res){
+	public static void affCategorie(){
 		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			Statement requete = laConnexion.createStatement();
+			ResultSet res = requete.executeQuery("SELECT * FROM `akniou1u_cpoa`.`Categorie`");
 			ArrayList<String> tCat = new ArrayList<String>();
 			while (res.next()){
 				String cat = new String();
-				cat = "('"+res.getInt("id_categorie")+"','"+res.getString("titre")+"','"+res.getString("visuel")+"')";
+				cat = "["+res.getInt("id_categorie")+", "+ res.getString("titre")+ ", "+res.getString("visuel")+"]";
 				tCat.add(cat);
 			}
-			System.out.println(tCat.toString());
+			for(int i=0; i<tCat.size(); i++) {
+				System.out.println(tCat.get(i));
+			}
 		}catch(SQLException sqle) {
 			System.out.println("Pb select"+sqle.getMessage());
 		}
--- cpoa_akniou/src/cpoa_akniou/test.java
+++ cpoa_akniou/src/cpoa_akniou/test.java
@@ -2,8 +2,7 @@ package cpoa_akniou;
 public class test {
 
 	public static void main(String[] args) {
-		tCategorie.editCategorie(9,"qdqdqd bg","zoroqdqd.png");
-
+		tCategorie.affCategorie();
 	}
-
+	
 }
