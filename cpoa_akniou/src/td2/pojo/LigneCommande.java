4f431cfa8f05029063582207e791c3c5598e18c9
akniou1u <akniou1u@iutm-inf-e37-07.iutmetz.site.univ-lorraine.fr>
2020-09-18 16:42:51 +0200

    classes produit et ligne commande complete

--- cpoa_akniou/src/td2/pojo/LigneCommande.java
+++ cpoa_akniou/src/td2/pojo/LigneCommande.java
@@ -1,5 +1,41 @@
 package td2.pojo;
 
 public class LigneCommande {
-
+	private int idCommande;
+	private int idProduit;
+	private int quantite;
+	private double tarifUnitaire;
+	
+	public LigneCommande(int idCommande, int idProduit, int quantite, double tarifUnitaire) {
+		super();
+		this.idCommande = idCommande;
+		this.idProduit = idProduit;
+		this.quantite = quantite;
+		this.tarifUnitaire = tarifUnitaire;
+	}
+	
+	public int getIdCommande() {
+		return idCommande;
+	}
+	public void setIdCommande(int idCommande) {
+		this.idCommande = idCommande;
+	}
+	public int getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	public double getTarifUnitaire() {
+		return tarifUnitaire;
+	}
+	public void setTarifUnitaire(double tarifUnitaire) {
+		this.tarifUnitaire = tarifUnitaire;
+	}
 }
--- cpoa_akniou/src/td2/pojo/Produit.java
+++ cpoa_akniou/src/td2/pojo/Produit.java
@@ -1,5 +1,71 @@
 package td2.pojo;
 
 public class Produit {
+	private int id;
+	private String nom;
+	private String description;
+	private double tarif;
+	private String visuel;
+	private int idCategorie;
+	
+	public Produit(int id, String nom, String description, double tarif, String visuel, int idCategorie) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.description = description;
+		this.tarif = tarif;
+		this.visuel = visuel;
+		this.idCategorie = idCategorie;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getTarif() {
+		return tarif;
+	}
+
+	public void setTarif(double tarif) {
+		this.tarif = tarif;
+	}
+
+	public String getVisuel() {
+		return visuel;
+	}
+
+	public void setVisuel(String visuel) {
+		this.visuel = visuel;
+	}
+
+	public int getId_categorie() {
+		return idCategorie;
+	}
+
+	public void setId_categorie(int id_categorie) {
+		this.idCategorie = id_categorie;
+	}
+	
+	
+	
 }
