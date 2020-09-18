90120127b51033267994be1714d74f77098fea31
akniou1u <akniou1u@iutm-inf-e37-07.iutmetz.site.univ-lorraine.fr>
2020-09-18 16:54:51 +0200

    debut classe daocategorie

--- cpoa_akniou/src/td2/dao/MySQLCategorieDAO.java
+++ cpoa_akniou/src/td2/dao/MySQLCategorieDAO.java
@@ -1,5 +1,18 @@
 package td2.dao;
 
-public class MySQLCategorieDAO {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import td2.connexion.Connexion;
+import td2.pojo.Categorie;
 
+public class MySQLCategorieDAO {
+	public static void insert(Categorie categorie) throws SQLException {
+		Connection laConnexion = Connexion.creeConnexion();
+		Statement requete = laConnexion.createStatement();
+		requete.executeUpdate("INSERT INTO `akniou1u_cpoa`.`Categorie` (`titre`, `visuel`) VALUES ('"+categorie.getTitre()+"', '"+categorie.getVisuel()+"');");
+		requete.close();
+		laConnexion.close();
+	}
+	
 }
