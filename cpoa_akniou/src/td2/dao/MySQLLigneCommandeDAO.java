ea98706854970a93d092ec227e94dfdc912bd04c
Mehdi <70698660+medkan01@users.noreply.github.com>
2020-09-19 14:16:11 +0200

    MySQLLigneCommandeDAO terminée

--- cpoa_akniou/src/td2/dao/MySQLLigneCommandeDAO.java
+++ cpoa_akniou/src/td2/dao/MySQLLigneCommandeDAO.java
@@ -1,5 +1,33 @@
 package td2.dao;
 
-public class MySQLLigneCommandeDAO {
+import java.sql.*;
 
+import td2.connexion.Connexion;
+import td2.pojo.LigneCommande;
+
+public class MySQLLigneCommandeDAO {
+	public static void insert(LigneCommande ligneCommande) throws SQLException{
+		Connection laConnexion = Connexion.creeConnexion();
+		Statement requete = laConnexion.createStatement();
+		requete.executeUpdate("INSERT INTO akniou1u.Ligne_commande (id_produit, quantite, tarif_unitaire) VALUES ('"+ligneCommande.getIdProduit()+"', '"+ligneCommande.getQuantite()+"', '"+ligneCommande.getTarifUnitaire()+"');");
+		requete.close();
+		laConnexion.close();
+	}
+	
+	public static void delete(LigneCommande ligneCommande) throws SQLException{
+		Connection laConnexion = Connexion.creeConnexion();
+		Statement requete = laConnexion.createStatement();
+		requete.executeUpdate("DELETE FROM akniou1u.Ligne_commande WHERE id_commande = '"+ligneCommande.getIdCommande()+"' AND id_produit ='"+ligneCommande.getIdProduit()+"';");
+		requete.close();
+		laConnexion.close();
+	}
+	
+	public static void update(LigneCommande ligneCommande) throws SQLException{
+		Connection laConnexion = Connexion.creeConnexion();
+		Statement requete = laConnexion.createStatement();
+		requete.executeUpdate("UPDATE akniou1u.Ligne_commande SET id_produit = '"+ligneCommande.getIdProduit()+"', quantite= '"+ligneCommande.getQuantite()+"', tarif_unitaire='"+ligneCommande.getTarifUnitaire()
+				+"' WHERE id_commande='"+ligneCommande.getIdCommande()+"' AND id_produit = '"+ligneCommande.getIdProduit()+"';");
+		requete.close();
+		laConnexion.close();
+	}
 }
