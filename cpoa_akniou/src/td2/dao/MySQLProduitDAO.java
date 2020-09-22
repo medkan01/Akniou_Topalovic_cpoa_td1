7d0ea506f1b7b6e626e76c0b5fea72e4f316224c
medkan01 <mehdiakniou@gmail.com>
2020-09-22 14:13:49 +0200

    MySQLProduitDAO updated

--- cpoa_akniou/src/td2/dao/MySQLProduitDAO.java
+++ cpoa_akniou/src/td2/dao/MySQLProduitDAO.java
@@ -7,26 +7,36 @@ import td2.pojo.Produit;
 public class MySQLProduitDAO {
 	public static void insert(Produit produit) throws SQLException{
 		Connection laConnexion = Connexion.creeConnexion();
-		Statement requete = laConnexion.createStatement();
-		requete.executeUpdate("INSERT INTO akniou1u.Produit (nom, description, tarif, visuel, id_categorie) VALUES ('"+produit.getNom()+"', '"+produit.getDescription()+"', '"
-					+produit.getTarif()+"','"+produit.getVisuel()+"', '"+produit.getIdCategorie()+"');");
+		PreparedStatement requete = laConnexion.prepareStatement(
+		"INSERT INTO akniou1u.Produit (nom, description, tarif, visuel, id_categorie) VALUES ('?', '?', '?','?', '?');");
+			requete.setString(1, produit.getNom());
+			requete.setString(2, produit.getDescription());
+			requete.setDouble(3, produit.getTarif());
+			requete.setString(4, produit.getVisuel());
+			requete.setInt(5, produit.getIdCategorie());
 		requete.close();
 		laConnexion.close();
 	}
 	
 	public static void delete(Produit produit) throws SQLException{
 		Connection laConnexion = Connexion.creeConnexion();
-		Statement requete = laConnexion.createStatement();
-		requete.executeUpdate("DELETE FROM akniou1u.Produit WHERE id_produit = '"+produit.getId()+"';");
+		PreparedStatement requete = laConnexion.prepareStatement(
+		"DELETE FROM akniou1u.Produit WHERE id_produit = '?';");
+			requete.setInt(1, produit.getId());
 		requete.close();
 		laConnexion.close();
 	}
 	
 	public static void update(Produit produit) throws SQLException{
 		Connection laConnexion = Connexion.creeConnexion();
-		Statement requete = laConnexion.createStatement();
-		requete.executeUpdate("UPDATE akniou1u.Produit SET nom = '"+produit.getNom()+"', description= '"+produit.getDescription()+"', tarif='"+produit.getTarif()+"', visuel='"+produit.getTarif()
-				+"', id_categorie='"+produit.getIdCategorie()+"' WHERE id_produit'"+produit.getId()+"';");
+		PreparedStatement requete = laConnexion.prepareStatement(
+		"UPDATE akniou1u.Produit SET nom = '?', description= '?', tarif='?', visuel='?', id_categorie='?' WHERE id_produit'?';");
+			requete.setString(1, produit.getNom());
+			requete.setString(2, produit.getDescription());
+			requete.setDouble(3, produit.getTarif());
+			requete.setString(4, produit.getVisuel());
+			requete.setInt(5, produit.getIdCategorie());
+			requete.setInt(6, produit.getId());
 		requete.close();
 		laConnexion.close();
 	}
