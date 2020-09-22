50e12a6e21b98c64ecb18ac6d1eccebcf133bfff
medkan01 <mehdiakniou@gmail.com>
2020-09-22 13:56:07 +0200

    MySQLLigneCommandeDAO updated

--- cpoa_akniou/src/td2/dao/MySQLLigneCommandeDAO.java
+++ cpoa_akniou/src/td2/dao/MySQLLigneCommandeDAO.java
@@ -7,25 +7,34 @@ import td2.pojo.LigneCommande;
 public class MySQLLigneCommandeDAO {
 	public static void insert(LigneCommande ligneCommande) throws SQLException{
 		Connection laConnexion = Connexion.creeConnexion();
-		Statement requete = laConnexion.createStatement();
-		requete.executeUpdate("INSERT INTO akniou1u.Ligne_commande (id_produit, quantite, tarif_unitaire) VALUES ('"+ligneCommande.getIdProduit()+"', '"+ligneCommande.getQuantite()+"', '"+ligneCommande.getTarifUnitaire()+"');");
+		PreparedStatement requete = laConnexion.prepareStatement(
+		"INSERT INTO akniou1u.Ligne_commande (id_produit, quantite, tarif_unitaire) VALUES ('?', '?', '?');");
+			requete.setInt(1, ligneCommande.getIdProduit());
+			requete.setInt(2, ligneCommande.getQuantite());
+			requete.setDouble(3, ligneCommande.getTarifUnitaire());
 		requete.close();
 		laConnexion.close();
 	}
 	
 	public static void delete(LigneCommande ligneCommande) throws SQLException{
 		Connection laConnexion = Connexion.creeConnexion();
-		Statement requete = laConnexion.createStatement();
-		requete.executeUpdate("DELETE FROM akniou1u.Ligne_commande WHERE id_commande = '"+ligneCommande.getIdCommande()+"' AND id_produit ='"+ligneCommande.getIdProduit()+"';");
+		PreparedStatement requete = laConnexion.prepareStatement(
+		"DELETE FROM akniou1u.Ligne_commande WHERE id_commande = '?' AND id_produit ='?';");
+			requete.setInt(1, ligneCommande.getIdCommande());
+			requete.setInt(2, ligneCommande.getIdProduit());
 		requete.close();
 		laConnexion.close();
 	}
 	
 	public static void update(LigneCommande ligneCommande) throws SQLException{
 		Connection laConnexion = Connexion.creeConnexion();
-		Statement requete = laConnexion.createStatement();
-		requete.executeUpdate("UPDATE akniou1u.Ligne_commande SET id_produit = '"+ligneCommande.getIdProduit()+"', quantite= '"+ligneCommande.getQuantite()+"', tarif_unitaire='"+ligneCommande.getTarifUnitaire()
-				+"' WHERE id_commande='"+ligneCommande.getIdCommande()+"' AND id_produit = '"+ligneCommande.getIdProduit()+"';");
+		PreparedStatement requete = laConnexion.prepareStatement(
+		"UPDATE akniou1u.Ligne_commande SET id_produit = '?', quantite= '?', tarif_unitaire='?' WHERE id_commande='?' AND id_produit = '?';");
+			requete.setInt(1, ligneCommande.getIdProduit());
+			requete.setInt(2, ligneCommande.getQuantite());
+			requete.setDouble(3, ligneCommande.getTarifUnitaire());
+			requete.setInt(1, ligneCommande.getIdCommande());
+			requete.setInt(2, ligneCommande.getIdProduit());
 		requete.close();
 		laConnexion.close();
 	}
