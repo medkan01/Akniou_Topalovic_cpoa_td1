c85e5f6e5c73fe05f94fac438ffaeaf1ec181f4a
medkan01Ã <mehdiakniou@gmail.com>
2020-09-25 20:46:25 +0200

    update

--- cpoa_akniou/src/td2/dao/MySQLCategorieDAO.java
+++ cpoa_akniou/src/td2/dao/MySQLCategorieDAO.java
@@ -41,14 +41,16 @@ public class MySQLCategorieDAO implements CategorieDAO{
 		return nbLignes == 1;
 	}
 	
-	public static void update(Categorie categorie) throws SQLException {
+	public boolean update(Categorie categorie) throws SQLException {
 		Connection c = Connexion.getInstance().getMaConnexion();
 		PreparedStatement requete = c.prepareStatement(
-			"UPDATE akniou1u_cpoa.Categorie SET titre = '?', visuel = '?' WHERE Categorie.id_categorie = '?';");
+			"UPDATE akniou1u_cpoa.Categorie SET titre = '?', visuel = '?' WHERE Categorie.id_categorie = '?';", Statement.RETURN_GENERATED_KEYS);
 				requete.setString(1, categorie.getTitre());
 				requete.setString(2, categorie.getVisuel());
 				requete.setInt(3, categorie.getId());
+		int nbLignes = requete.executeUpdate();
 		requete.close();
+		return nbLignes == 1;
 	}
 
 	public Categorie getById(int id) throws SQLException{
