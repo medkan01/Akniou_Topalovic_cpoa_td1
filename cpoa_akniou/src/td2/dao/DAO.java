0f1fbc3a667bed7beb93c4d8280f001de26dd8e6
medkan01Ã <mehdiakniou@gmail.com>
2020-09-29 22:37:20 +0200

    adding getAll method

--- cpoa_akniou/src/td2/dao/DAO.java
+++ cpoa_akniou/src/td2/dao/DAO.java
@@ -1,9 +1,11 @@
 package td2.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface DAO<T> {
     public T getById(int id) throws SQLException;
+    public ArrayList<T> getAll() throws SQLException;
     public boolean insert(T objet) throws SQLException;
     public boolean delete(T objet) throws SQLException;
     public boolean update(T objet) throws SQLException;
