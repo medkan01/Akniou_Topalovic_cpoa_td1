d9fc14d2de5f6bdf2bb8deae586f93abe032f68a
medkan01Ã <mehdiakniou@gmail.com>
2020-09-30 14:21:07 +0200

    update

--- cpoa_akniou/src/td2/main/ProgrammePrincipal.java
+++ cpoa_akniou/src/td2/main/ProgrammePrincipal.java
@@ -1,8 +1,10 @@
 package td2.main;
 
+import td2.vue.VuePrincipale;
+
 public class ProgrammePrincipal {
     public static void main(String args[]){
-        
+        VuePrincipale.selection();
     }
 
 }
