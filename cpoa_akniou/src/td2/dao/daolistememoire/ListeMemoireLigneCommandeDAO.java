7747a05eb235a43acdcb0f2f17d33fbaa834be0d
medkan01 <mehdiakniou@gmail.com>
2020-10-02 10:49:26 +0200

    1

--- cpoa_akniou/src/td2/dao/daolistememoire/ListeMemoireLigneCommandeDAO.java
+++ cpoa_akniou/src/td2/dao/daolistememoire/ListeMemoireLigneCommandeDAO.java
@@ -1,11 +1,21 @@
 package td2.dao.daolistememoire;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import td2.dao.daofactory.DAOFactory;
+import td2.dao.daofactory.Persistance;
+import td2.pojo.Commande;
 import td2.pojo.LigneCommande;
+import td2.pojo.Produit;
 
 public class ListeMemoireLigneCommandeDAO {
-    
+    private ListeMemoireCommandeDAO commandeInstance = ListeMemoireCommandeDAO.getInstance();
+    private ListeMemoireProduitDAO produitInstance = ListeMemoireProduitDAO.getInstance();
+    private static DAOFactory daos = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
     private static ListeMemoireLigneCommandeDAO instance;
-
+    private List<Commande> donnees;
+    
 	public static ListeMemoireLigneCommandeDAO getInstance(){
 		if (instance == null){
 			instance = new ListeMemoireLigneCommandeDAO();
@@ -14,11 +24,34 @@ public class ListeMemoireLigneCommandeDAO {
     }
     
     private ListeMemoireLigneCommandeDAO(){
-
+        donnees = new ArrayList<Commande>();
+        donnees = commandeInstance.getAll();
     }
 
-    public boolean insert(int idProduit, int idCommande, LigneCommande objet){
-        
+    public boolean insert(int idCommande, int idProduit, LigneCommande objet){
+        Commande commande = commandeInstance.getById(idCommande);
+        Produit produit= produitInstance.getById(idProduit);
+        commande.ajouterLigne(produit, objet);
+        commandeInstance.update(commande);
+        return true;
+    }
 
+    public boolean update(int idCommande, int idProduit, LigneCommande objet){
+        Commande commande = commandeInstance.getById(idCommande);
+        Produit produit= produitInstance.getById(idProduit);
+        commande.modifierLigne(produit, objet);
+        commandeInstance.update(commande);
+        return true;
+    }
+    public boolean delete(int idCommande, int idProduit){
+        Commande commande = commandeInstance.getById(idCommande);
+        Produit produit= produitInstance.getById(idProduit);
+        commande.supprimerLigne(produit);
+        commandeInstance.update(commande);
+        return true;
+    }
+
+    public ArrayList<Commande> getAll(int idCommande){
+        ArrayList<Commande>
     }
 }
