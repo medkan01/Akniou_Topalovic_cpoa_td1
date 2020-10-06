8d3cdcb00053800ed83d4bd904ac04824a0061ec
medkan01Ã <mehdiakniou@gmail.com>
2020-10-06 21:55:40 +0200

    MySQLLigneCommandeDAOTest

---
+++ cpoa_akniou/src/td2/tests/daomysqltests/MySQLLigneCommandeDAOTest.java
@@ -0,0 +1,46 @@
+package td2.tests.daomysqltests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.sql.SQLException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import td2.dao.daomysql.MySQLLigneCommandeDAO;
+import td2.pojo.LigneCommande;
+public class MySQLLigneCommandeDAOTest {
+
+    private MySQLLigneCommandeDAO instance;
+    private LigneCommande ligneCommande;
+
+    @BeforeEach
+    public void setUp() {
+        this.instance = MySQLLigneCommandeDAO.getInstance();
+        this.ligneCommande = new LigneCommande(1,1);
+    }
+
+	@Test
+	void testInsert() {
+		try{
+		    assertTrue(this.instance.insert(1,1,this.ligneCommande));
+		} catch (SQLException sqle) {
+			System.out.println("Erreur \n" + sqle.getMessage());
+		}
+    }
+    
+    @Test
+    void testDelete() {
+        try {
+            assertTrue(this.instance.delete(1, 1));
+        } catch(SQLException sqle) {
+            System.out.println("Erreur:\n" +sqle.getMessage());
+        }
+    }
+
+    @Test
+    void testUpdate() {
+        try {
+            assertTrue(this.instance.update(1, 1, this.ligneCommande));
+        } catch(SQLException sqle) {
+            System.out.println("Erreur:\n" + sqle.getMessage());
+        }
+    }
+}
