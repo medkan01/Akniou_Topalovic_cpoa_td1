74b191028c2bf50ba139d58e02e46794427da4c8
Ajdin <ajdintpc@gmail.com>
2020-10-13 20:57:52 +0200

    file ajoutProduit.fxml update

--- cpoa_akniou/src/td2/controller/AjoutProduitController.java
+++ cpoa_akniou/src/td2/controller/AjoutProduitController.java
@@ -1,7 +1,15 @@
 package td2.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class AjoutProduitController {
+
+    @FXML
+    private Label labelResumeProduit;
+    
     public void CreerProduit(){
-        
+        this.labelResumeProduit.setText("ok");
     }
+
 }
