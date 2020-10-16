745a21109b84d05836092d9dc8232f8342564cd3
Ajdin <ajdintpc@gmail.com>
2020-10-16 21:21:02 +0200

    page accueil

--- cpoa_akniou/src/td2/application/Main.java
+++ cpoa_akniou/src/td2/application/Main.java
@@ -15,13 +15,13 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) {
         try {
-            URL fxmlURL=getClass().getResource("../javafx/AjoutCategorie.fxml");
+            URL fxmlURL=getClass().getResource("../javafx/MenuPrincipal.fxml");
             FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
             Node root = fxmlLoader.load();
-            Scene scene = new Scene((AnchorPane) root, 680, 550);
+            Scene scene = new Scene((AnchorPane) root, 980, 650);
             //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
-            primaryStage.setTitle("Ajout Categorie");
+            primaryStage.setTitle("EZ.Gestion");
             primaryStage.show();
             primaryStage.setResizable(false);
         } catch (Exception e) {
