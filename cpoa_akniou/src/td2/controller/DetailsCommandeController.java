3788490faf240e135652c2ec6ee88fec9a3ac64b
medkan01 <mehdiakniou@gmail.com>
2020-11-02 17:38:24 +0100

    update

--- cpoa_akniou/src/td2/controller/DetailsCommandeController.java
+++ cpoa_akniou/src/td2/controller/DetailsCommandeController.java
@@ -1,10 +1,8 @@
 package td2.controller;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -22,9 +20,7 @@ import td2.pojo.Produit;
 import td2.pojo.ProduitSelectionne;
 
 public class DetailsCommandeController{
-    
-    private int idCom;
-    private LocalDate date;
+
     private DAOFactory daos;
     @FXML private TableView<ProduitSelectionne> tableProduitSelectionne;
     @FXML private Label labelIdCommande, labelDateCommande, labelIdClient;
@@ -34,8 +30,6 @@ public class DetailsCommandeController{
 	public void setCommande(Commande obj){
         try{
             Commande commande = obj;
-            this.idCom = commande.getId();
-            this.date = commande.getDate();
             HashMap<Produit, LigneCommande> listeLigneCommande = commande.getLigneCommande();
             ArrayList<ProduitSelectionne> listeProduitSelectionnes = new ArrayList<ProduitSelectionne>();
             Set<Produit> keys = listeLigneCommande.keySet();
